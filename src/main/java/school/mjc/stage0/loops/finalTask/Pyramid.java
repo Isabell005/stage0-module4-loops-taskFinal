22cf4400f5a0ab86694d46a50adb681b8ac8c354
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-27 11:56:28 +0400

    stage0module4loopsTaskFinal

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,9 +3,20 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
 
+
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
+        for (int i = 7; i >= 1; i--) {
+            // white spaces in the front of the numbers
+            int numberOfWhiteSpaces = i * 2;
+
+            System.out.println(" " + numberOfWhiteSpaces);
+            for (int x = i; x == i; i--) {
+                System.out.print(i + " ");
+            }
+            System.out.println("");
+        }
     }
 }
